a2ce2d64bd3a6911dead2b49ef37bd46b8a12c24
大髙知三佳 <hiyoko_m@icloud.com>
2025-03-26 19:08:31 +0900

    コードの修正

--- mdn.js
+++ mdn.js
@@ -1,6 +1,6 @@
 const today = new Date();
-const birthday = new Date("December 17, 1995 03:24:00"); // 避けてください。すべてのランタイムで動作するとは限りません。
-const birthday2 = new Date("1995-12-17T03:24:00"); // これは標準化されており、動作が信頼できます
-const birthday3 = new Date(1995, 11, 17); // 月は 0 起点です
-const birthday4 = new Date(1995, 11, 17, 3, 24, 0);
+const birthday = new Date();
+const birthday2 = new Date();
+const birthday3 = new Date();
+const birthday4 = new Date();
 const birthday5 = new Date
