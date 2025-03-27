4c10be2d15d37afaaba323234c287b47fba1378c
大髙知三佳 <hiyoko_m@icloud.com>
2025-03-27 20:04:40 +0900

    課題を提出

---
+++ square.js
@@ -0,0 +1,6 @@
+const square = (number) => {  // number という引数を受け取る
+    return number * number;  // その引数の二乗を計算して返す
+}
+
+const result = square(10);  // 引数に10を渡して関数を実行
+console.log(result);  // 結果（100）をコンソールに表示
